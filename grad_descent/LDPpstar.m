2cf8b6519828f0b6f26417c2f2bb5e8f68320a0b
Roshan Rao <roshan_rao@brown.edu>
2016-04-24 13:06:41 -0400

    commented matlab code

--- grad_descent/LDPpstar.m
+++ grad_descent/LDPpstar.m
@@ -1,43 +1,63 @@
 function lambda = LDPpstar(hx, Tx_all, theta, accuracy, lambda)
-% Inputs:
-    % hx => an (s, 1)-dimensional probability vector
-    % Tx => an (s, c)-dimensional vector of sufficient statistics
-    % theta => an (s, 1)-dimensional constraint vector
-    % accuracy => level of accuracy (e.g. 1e-3)
+% Dimensions:
+%    n => number of grid points
+%    c => number of features
+%    s => number of commands
     
-    % Optional Arguments
-    % lambda => a value of lambda to start with
+% Inputs:
+%    hx => an (n, 1)-dimensional probability vector
+%    Tx => an (n, c)-dimensional vector of sufficient statistics
+%    theta => a (c, 1)-dimensional constraint vector
+%    accuracy => level of accuracy (e.g. 1e-3)
+    
+%    Optional Arguments
+%        lambda => initialized value of lambda
+%    
+% Variables:
+%    lambda => a (c, 1)-dimensional weight vector
+%    zlambda => a (s, 1)-dimensional vector of normalization constants,
+%               one for every command (because each command produces a 
+%               different distribution)
+%    expvals => a (c, 1)-dimensional vector giving the expected value of
+%               the sufficient statistics across all commands for a given 
+%               lambda. Goal is to get this to be close to theta.
 
+% If no initialization for lambda, initialize to zero.
 if nargin < 5
     c = size(Tx_all{1}, 2);
     lambda = zeros(c, 1);
 end
+
 zlambda = get_zlambda(hx, lambda, Tx_all);
 expvals = get_expvals(hx, lambda, zlambda, Tx_all);
-epsilon = 0.01;
+epsilon = 0.01; % step size
 
+% While L2 norm between expected value of features across all commands for
+% a given lambda and true average value is greater than accuracy,
 while (norm(expvals - theta) > accuracy)
     disp(['Error = ' num2str(norm(expvals - theta))])
-    lambda = lambda - epsilon*(expvals - theta);
-    zlambda = get_zlambda(hx, lambda, Tx_all);
-    expvals = get_expvals(hx, lambda, zlambda, Tx_all);
+    lambda = lambda - epsilon*(expvals - theta); % grad descent
+    zlambda = get_zlambda(hx, lambda, Tx_all); % recalculate all normalization constants
+    expvals = get_expvals(hx, lambda, zlambda, Tx_all); % recalculate expected values
 end
 end
 
 function expvals = get_expvals(hx, lambda, zlambda, Tx_all)
+% Calculates expected values for a given lambda
 c = size(Tx_all{1}, 2);
 expvals = zeros(c, 1);
-num_scenes = length(Tx_all);
-for i = 1:num_scenes
+num_commands = length(Tx_all);
+for i = 1:num_commands
     expvals = expvals + sum(Tx_all{i}.*repmat(hx.*exp(Tx_all{i}*lambda)/zlambda(i), 1, c))';
 end
-expvals = expvals/num_scenes;
+expvals = expvals/num_commands;
 end
 
 function zlambda = get_zlambda(hx, lambda, Tx_all)
-num_scenes = length(Tx_all);
-zlambda = zeros(num_scenes, 1);
-for i = 1:num_scenes
+% Calculates normalization constant for all commands for a given lambda
+num_commands = length(Tx_all);
+zlambda = zeros(num_commands, 1);
+for i = 1:num_commands
     zlambda(i) = sum(exp(Tx_all{i}*lambda).*hx);
 end
 end
--- grad_descent/refex_test.m
+++ grad_descent/refex_test.m
@@ -1,3 +1,9 @@
+% Tx_all => size 12 cell array. Each cell contains the value of the
+%           features on the discretization of the board for a given command.
+% all_theta => average value of features at every data point (over all commands).
+% hx => uniform distribution over all data points
+% 
+
 Tx_all = {};
 all_theta = zeros(3, 1);
 for i = 1:12
@@ -8,6 +14,10 @@ end
 all_theta = all_theta/12;
 hx = ones(length(Tx_all{1}), 1)/length(Tx_all{1});
 
+% if you preset a lambda, it won't be overwritten. This is helpful b/c you
+% can initialize lambda to some value so you don't always have to start at
+% the beginning.
 if ~exist('lambda', 'var'); lambda = zeros(size(Tx_all{i}, 2), 1); end;
+
 lambda = LDPpstar(hx, Tx_all, all_theta, 0.001, lambda);
 disp(lambda)
